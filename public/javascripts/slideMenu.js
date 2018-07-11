5e9bd4585b12b85116214707f3f9ef75a53b5a97
Vipul Karanjkar <vipulkaranjkar94@gmail.com>
2018-07-10 20:02:51 -0700

    signIn route

---
+++ public/javascripts/slideMenu.js
@@ -0,0 +1,21 @@
+function openSlideMenu(){
+    document.getElementById('side-menu').style.width = '200px';
+    document.getElementById('about').style.marginLeft = '200px';
+    document.getElementById('services').style.marginLeft = '200px';
+    document.getElementById('portfolio').style.marginLeft = '200px';
+    document.getElementById('myCarousel').style.marginLeft = '200px';
+    document.getElementById('pricing').style.marginLeft = '200px';
+    document.getElementById('contact').style.marginLeft = '200px';
+    document.getElementById('footer').style.marginLeft = '200px';
+}
+
+function closeSlideMenu(){
+    document.getElementById('side-menu').style.width = '0';
+    document.getElementById('about').style.marginLeft = '0';
+    document.getElementById('services').style.marginLeft = '0';
+    document.getElementById('portfolio').style.marginLeft = '0';
+    document.getElementById('myCarousel').style.marginLeft = '0';
+    document.getElementById('pricing').style.marginLeft = '0';
+    document.getElementById('contact').style.marginLeft = '0';
+    document.getElementById('footer').style.marginLeft = '0';
+}
